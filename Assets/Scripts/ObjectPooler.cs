c063173b06863b540f49f8a8298ebfc48ada1889
AlexanderIvkin <dvakota@bk.ru>
2024-05-15 22:10:39 +0600

    Summary

--- Assets/Scripts/ObjectPooler.cs
+++ Assets/Scripts/ObjectPooler.cs
@@ -4,43 +4,43 @@ using UnityEngine;
 
 public class ObjectPooler : MonoBehaviour
 {
-    [SerializeField] private GameObject _object;
+    [SerializeField] private Cube _objectPrefab;
     [SerializeField] private int _count;
 
-    private List<GameObject> _objects = new();
-    private List<GameObject> _freeObjects = new();
+    private List<Cube> _objects = new();
+    private List<Cube> _freeObjects = new();
 
     private void Awake()
     {
         for (int i = 0; i < _count; i++)
         {
-            GameObject newObject = Instantiate(_object);
-            newObject.SetActive(false);
-            _objects.Add(newObject);
+            Cube newCube = CreateObject();
+            newCube.gameObject.SetActive(false);
+            _objects.Add(newCube);
         }
     }
 
-    private GameObject CreateObject()
+    private Cube CreateObject()
     {
-        return Instantiate(_object);
+        return Instantiate(_objectPrefab);
     }
 
     public void Get(Vector3 positon)
     {
-        if (TryGetFreeObject(out GameObject freeObject) == false)
+        if (TryGetFreeObject(out Cube freeObject) == false)
         {
             freeObject = CreateObject();
         }
 
         freeObject.transform.position = positon;
-        freeObject.SetActive(true);
+        freeObject.gameObject.SetActive(true);
     }
 
-    private bool TryGetFreeObject(out GameObject freeObject)
+    private bool TryGetFreeObject(out Cube freeObject)
     {
         freeObject = null;
 
-        _freeObjects = _objects.Where(obj => obj.active == false).ToList();
+        _freeObjects = _objects.Where(obj => obj.gameObject.active == false).ToList();
 
         bool isExist = _freeObjects.Count > 0;
 
